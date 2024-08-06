75f788bb25a88e4987e6fbdda2619f0d4652bd5d
Alice Tonkopiy <adtonkopiy@gmail.com>
2024-08-06 17:37:30 +0200

    new version ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,39 +1,54 @@
 #include "PmergeMe.hpp"
 
-int strToInt(const std::string &str)
+void checkInput(int argc, char **argv)
 {
-    std::istringstream ss(str);
-    int ret;
-    ss >> ret;
-    return ret;
-}
-std::pair<size_t, size_t> makePairs(std::size_t n)
-{
-    for (size_t i = 1; i < 33; i++)
+    for (int i = 1; i < argc; ++i)
     {
-        if (n < jacobsthalNum[i])
-            return std::make_pair(jacobsthalNum[i - 1], jacobsthalNum[i]);
+        std::string str(argv[i]);
+        if (str.find_first_not_of("-0123456789") != str.npos)
+            throw std::invalid_argument("invalid input: not a number");
+        double value = strtod(argv[i], 0);
+        if (value > INT_MAX || value < 1)
+            throw std::invalid_argument("invalid input: not within INT range, negavite number or 0");
     }
-    throw std::out_of_range("n is too large to find a pair in Jacobsthal numbers");
-}
+};
+
+void printArr(char** argv)
+{
+    int i = 1;
+    while (argv[i])
+	{
+        std::string input = argv[i];
+	    std::cout << input << " ";
+        i++;
+    }
+    std::cout << std::endl;
+};
+
+bool custom_cmp(const std::pair<int, int> &a, const std::pair<int, int> &b) {
+    return a.first < b.first;
+};
 
 void displaySort(int argc, char **argv)
 {
-    std::vector<int> vec;
-    std::clock_t startVec = std::clock();
+    std::vector< std::pair<int, int> > vec;
+    std::vector<int> main_chain;
+    int additional_value = -1;
     try
     {
-        populateArr(vec, argc, argv);
+        checkInput(argc, argv);
     }
     catch (std::exception &e)
     {
         std::cout << "Error: " << e.what() << std::endl;
         return;
     }
-    printArr("Before", vec);
+    std::cout << "Before: " << std::flush;
+    printArr(argv);
+    std::clock_t startVec = std::clock();
     try
     {
-        mergeInsertSort(vec);
+        mergeInsertSort(vec, main_chain, argv, additional_value);
     }
     catch (std::exception &e)
     {
@@ -41,22 +56,15 @@ void displaySort(int argc, char **argv)
         return;
     }
     std::clock_t endVec = std::clock();
-    printArr("After", vec);
-    
-    std::deque<int> dec;
+    std::cout << "After: " << std::flush;
+    print_after(main_chain);
+
+    std::deque< std::pair<int, int> > dec;
+    std::deque<int> main_chain_dec;
     std::clock_t startDec = std::clock();
-    try 
-    {
-        populateArr(dec, argc, argv);
-    }
-    catch (std::exception &e)
-    {
-        std::cout << "Error: " << e.what() << std::endl;
-        return;
-    }
     try
     {
-        mergeInsertSort(dec);
+        mergeInsertSort(dec, main_chain_dec, argv, additional_value);
     }
     catch (std::exception &e)
     {
@@ -64,6 +72,9 @@ void displaySort(int argc, char **argv)
         return;
     }
     std::clock_t endDec = std::clock();
+    // std::cout << "After: " << std::flush;
+    // print_after(main_chain_dec);
     printTime(startVec, endVec, argc, vec);
     printTime(startDec, endDec, argc, dec);
 }
+
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -11,24 +11,26 @@
 #include <typeinfo>
 #include <iomanip>
 #include <climits>
+#include <list>
 
 const std::size_t jacobsthalNum[33] = {
     0, 1, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461,
     10923, 21845, 43691, 87381, 174763, 349525, 699051, 1398101,
     2796203, 5592405, 11184811, 22369621, 44739243, 89478485,
     178956971, 357913941, 715827883, 1431655765};
-typedef std::vector<int>::iterator iterator;
-int strToInt(const std::string &str);
+void checkInput(int argc, char **argv);
+void printArr(char** argv);
 void displaySort(int argc, char **argv);
-std::pair<size_t, size_t> makePairs(std::size_t n);
+bool custom_cmp(const std::pair<int, int> &a, const std::pair<int, int> &b);
 
-template <typename T>
-void printArr(std::string str, T &arr)
+template <typename ContainerMain>
+void print_after(ContainerMain main_chain)
 {
-    std::cout << str << ": ";
-    for (iterator it = arr.begin(); it != arr.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
+	for (typename ContainerMain::iterator it = main_chain.begin(); it != main_chain.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << '\n';
 }
 
 template <typename T>
@@ -37,111 +39,131 @@ void printTime(std::clock_t start, std::clock_t end, int argc, T &container)
     double duration = 1000000.0 * static_cast<double>(end - start) / CLOCKS_PER_SEC;
     std::string name = typeid(container).name();
     name.erase(0, 3);
-    name.erase(name.size() - 8);
+    name.erase(name.size() - 20);
     std::cout << "Time to process a range of " << (argc - 1) << " elements with std::" << name << ":  " << std::fixed << std::setprecision(2) << duration << " us" << std::endl;
 }
 
-template <typename T>
-void populateArr(T &arr, int argc, char **argv)
-{
-    for (int i = 1; i < argc; ++i)
-    {
-        std::string str(argv[i]);
-        if (str.find_first_not_of("-0123456789") != str.npos)
-            throw std::invalid_argument("invalid input: not a number");
-        double value = strtod(argv[i], 0);
-        if (value > INT_MAX || value < 1)
-            throw std::invalid_argument("invalid input: not within INT range, negavite number or 0");
-        arr.push_back(strToInt(argv[i]));
-    }
+// Do merge sort with recursive approach splitting the list in half each time and merging them back together
+template <typename ContainerPairs>
+void mergeSort(typename ContainerPairs::iterator start, typename ContainerPairs::iterator end, size_t size) {
+    if (size == 0 && start != end)
+        size = std::distance(start, end);
+    if (size <= 1)
+        return;
+
+    size_t firstHalf = size / 2;
+    size_t secondHalf = size - firstHalf;
+    typename ContainerPairs::iterator center = start;
+    // Move the iterator to the center of the list
+    std::advance(center, firstHalf);
+
+    mergeSort<ContainerPairs>(start, center, firstHalf);
+    mergeSort<ContainerPairs>(center, end, secondHalf);
+    std::inplace_merge(start, center, end, custom_cmp);
 }
 
-template <typename Container>
-Container setJakobSeqV(size_t n)
-{
-    Container jSeq;
-    std::pair<size_t, size_t> pair = makePairs(n);
-    if (pair.first > pair.second)
-    {
-        std::swap(pair.first, pair.second);
-    }
+// Do a binary search to find the correct position within the slice of the main chain and insert the value
+template <typename ContainerMain>
+void binary_search_insertion(ContainerMain *main_chain, typename ContainerMain::iterator end, int val) {
+    typename ContainerMain::iterator place_to_insert = std::lower_bound(main_chain->begin(), end, val);
+    main_chain->insert(place_to_insert, val);
 
-    for (size_t i = 0; i < pair.second; ++i)
-    {
-        jSeq.push_back(i);
+    /*
+    // print main chain
+    std::cout << "main chain with current value: " << val << ": "
+              << "end_interator: " << *end << " place_to_insert: " << *(place_to_insert--) << std::endl;
+    for (typename ContainerMain::iterator it = main_chain->begin(); it != main_chain->end(); it++) {
+        std::cout << *it << " ";
     }
-    return jSeq;
+    std::cout << '\n';
+    */
 }
 
-template <typename Container>
-void mergeVector(Container &arr, int left, int mid, int right)
-{
-    size_t n1 = mid - left + 1;
-    size_t n2 = right - mid;
+template <typename ContainerMain, typename ContainerPairs>
+void insert_into_main_chain(ContainerPairs listy, ContainerMain *main_chain, int additional_value) {
+    int jacobsthal_idx = 1;
 
-    Container L(arr.begin() + left, arr.begin() + mid + 1);
-    Container R(arr.begin() + mid + 1, arr.begin() + right + 1);
+    typename ContainerMain::iterator slice_delim_it;
+    typename ContainerPairs::iterator pair_it;
+    typename ContainerPairs::iterator last_jacob_it = listy.begin();
 
-    Container jakobSeq = setJakobSeqV<Container>(n1 + n2);
-    size_t idx = 0;
-    size_t i = 0;
-    size_t j = 0;
-    size_t k = left; // Start merging from the original 'left' index
+    while (jacobsthalNum[jacobsthal_idx] <= listy.size()) {
+        pair_it = listy.begin();
+        std::advance(pair_it, jacobsthalNum[jacobsthal_idx] - 1);
+        last_jacob_it = pair_it;
 
-    while (i < n1 && j < n2)
-    {
-        size_t a = jakobSeq[idx++];
-        size_t b = jakobSeq[idx++];
+        // how many pend elements have been inserted
+        int insertion_counter = 0;
+        while (jacobsthalNum[jacobsthal_idx] - insertion_counter > jacobsthalNum[jacobsthal_idx - 1]) {
+            slice_delim_it = std::find(main_chain->begin(), main_chain->end(), pair_it->second);
+            binary_search_insertion(main_chain, slice_delim_it, pair_it->first);
+            pair_it--;
+            insertion_counter++;
+        }
+        jacobsthal_idx++;
+    }
 
-        for (size_t count = a; count <= b; ++count)
-        {
-            if (i < n1 && j < n2)
-            {
-                if (L[i] <= R[j])
-                {
-                    arr[k++] = L[i++];
-                }
-                else
-                {
-                    arr[k++] = R[j++];
-                }
-            }
-            else if (i < n1)
-            {
-                arr[k++] = L[i++];
-            }
-            else if (j < n2)
-            {
-                arr[k++] = R[j++];
-            }
+    // if the Jacobsthal number is larger than the size of the list, insert the remaining elements
+    if (jacobsthalNum[jacobsthal_idx] != listy.size()) {
+        pair_it = listy.end();
+        if (pair_it != listy.begin())
+            pair_it--;
+        while (pair_it != last_jacob_it) {
+            slice_delim_it = std::find(main_chain->begin(), main_chain->end(), pair_it->second);
+            binary_search_insertion(main_chain, slice_delim_it, pair_it->first);
+            pair_it--;
         }
     }
-    // Copy remaining elements of L and R if any
-    while (i < n1)
-    {
-        arr[k++] = L[i++];
-    }
-    while (j < n2)
-    {
-        arr[k++] = R[j++];
-    }
-}
-template <typename Container>
-void insertVector(Container &arr, int left, int right)
-{
-    if (left < right)
-    {
-        int mid = left + (right - left) / 2;
-        insertVector(arr, left, mid);
-        insertVector(arr, mid + 1, right);
-        mergeVector(arr, left, mid, right);
+
+    if (additional_value != -1) {
+        binary_search_insertion(main_chain, main_chain->end(), additional_value);
     }
 }
 
-template <typename Container>
-void mergeInsertSort(Container &arr)
-{
-    if (arr.size() < 2)
-        return;
-    insertVector(arr, 0, arr.size() - 1);
+template <typename ContainerMain, typename ContainerPairs>
+void init_main_chain(ContainerMain *main_chain, ContainerPairs listy) {
+    for (typename ContainerPairs::iterator it = listy.begin(); it != listy.end(); ++it)
+        main_chain->push_back(it->second);
+
+    // the first number of pend can always be inserted as the first number in the main chain.
+    // it's always smaller than it's paired number
+    if (!listy.empty())
+        main_chain->insert(main_chain->begin(), listy.begin()->first);
+}
+
+
+template <typename ContainerPairs>
+void sort_pairs(ContainerPairs *listy) {
+    for (typename ContainerPairs::iterator it = listy->begin(); it != listy->end(); ++it) {
+        if (it->first > it->second) {
+            int temp = it->first;
+            it->first = it->second;
+            it->second = temp;
+        }
+    }
+}
+
+template <typename ContainerPairs>
+void create_pairs(ContainerPairs *listy, char **argv, int *additional_value) {
+    int i = 1;
+
+    while (argv[i]) {
+        if (argv[i] && argv[i + 1]) {
+            std::pair<int, int> pair_temp(std::atoi(argv[i]), std::atoi(argv[i + 1]));
+            listy->push_back(pair_temp);
+            i += 2;
+        } else {
+            *additional_value = std::atoi(argv[i]);
+            i++;
+        }
+    }
+}
+
+template <typename ContainerPairs, typename ContainerMain>
+void mergeInsertSort(ContainerPairs &arr, ContainerMain &main_chain, char **argv, int additional_value) {
+    create_pairs(&arr, argv, &additional_value);
+    sort_pairs(&arr);
+    mergeSort<ContainerPairs>(arr.begin(), arr.end(), 0);
+    init_main_chain(&main_chain, arr);
+    insert_into_main_chain(arr, &main_chain, additional_value);
 }
